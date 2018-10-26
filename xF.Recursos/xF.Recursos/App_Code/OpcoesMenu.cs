cb806ddfc04b3f0c00641f77f556918d3594bd92
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-25 23:11:28 -0300

    g

---
+++ xF.Recursos/xF.Recursos/App_Code/OpcoesMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xF.Recursos.App_Code
+{
+    class OpcoesMenu
+    {
+        public string Descricao { get; set; }
+        public string Icone { get; set; }
+        public Type TargetType { get; set; }
+
+    }
+}
---
+++ xF.Recursos/xF.Recursos/Menu/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xF.Recursos.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            menuPage.ListaMenu.ItemSelected += ListaMenu_ItemSelected;
+		}
+
+        private void ListaMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as App_Code.OpcoesMenu;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                menuPage.ListaMenu.SelectedItem = null;
+                this.IsPresented = false;
+            }
+        }
+    }
+}
